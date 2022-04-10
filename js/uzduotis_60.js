ec7d031f92ca3ebcf1427da74e4f66a3ffc0f6f8
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-04-10 19:31:09 +0300

    Add files via upload

---
+++ js/uzduotis_60.js
@@ -0,0 +1,36 @@
+let f_name  = "Alex";
+console.log(f_name);
+let l_name  = "Smith";
+console.log(l_name);
+let age = 40;
+console.log(age);
+let status = true;
+console.log(status);
+const house = 1;
+console.log(house);
+let nameSurname = f_name + " " + l_name;
+console.log(nameSurname);
+let nameAndAge = f_name + " " + age;
+console.log(nameAndAge);
+let ageThree = age ** 3;
+console.log(ageThree);
+console.log(age / 10);
+console.log(age / 4);
+console.log(typeof name);
+console.log(typeof age);
+var mood = "happy";
+console.log(mood);
+mood = "sad";
+console.log(mood);
+console.log(f_name[1], f_name[2], f_name[3]);
+console.log("aaa aaa aaa \t bbb bbb \n ccc ccc ccc");
+let fullName = `${f_name} \n ${l_name} ${age}`;
+console.log(fullName);
+console.log("5" == 5);
+console.log("5" === 5);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 !== false);
+console.log(0 != false);
+
+
