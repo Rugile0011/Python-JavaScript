b2e8b1ab5e9b4fbac5e80f92b0ddd7c2f13c5357
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:58:19 +0300

    Update 2.5.py

--- Homework/2.5.py
+++ Homework/2.5.py
@@ -1,6 +1,6 @@
-metai = int(input("Iveskite metus, kuriuos norite patikrinti: "))
+year = int(input("Enter the year you want to check: "))
 
-if ((metai%400 == 0)or ((metai%4 == 0 ) and (metai%100 != 0))):
-    print("%d Keliamieji" %metai)
+if ((year%400 == 0)or ((year%4 == 0 ) and (year%100 != 0))):
+    print("%d Leap year" %year)
 else:
-    print("%d Nekeliamieji" %metai)
+    print("%d Not leap year" %year)
