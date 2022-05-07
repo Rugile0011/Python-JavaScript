9ef19b747313fbb7ffc07745beeb4dbe5b422ee5
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:16:25 +0300

    Update 19.2.py

--- Homework/19.2.py
+++ Homework/19.2.py
@@ -1,45 +1,45 @@
 from tkinter import *
 
-langas = Tk()
-issaugota = StringVar()
+window = Tk()
+saved = StringVar()
 
-def spausdinti(event):
-    ivesta = ivesti.get()
-    issaugota.set(ivesta)
-    rezultatas['text'] = f'Labas,{ivesta} !'
-
-
-uzrasas = Label(langas, text="Iveskite vardą")
-ivesti = Entry(langas)
-mygtukas = Button(langas, text='Patvirtinti', fg="white", bg="pink", command=spausdinti)
-langas.bind('<Return>', lambda event: spausdinti())
-rezultatas = Label(langas, text="")
-
-mygtukas.bind("<Button-1>", spausdinti)
-langas.bind("<Return>", spausdinti)
-
-uzrasas.grid(row=0, column=0)
-ivesti.grid(row=0, column=1)
-mygtukas.grid(row=0, column=2)
-rezultatas.grid(row=1, columnspan=3)
+def printing(event):
+    input = input.get()
+    saved.set(input)
+    result['text'] = f'Labas,{input} !'
 
 
-meniu = Menu(langas)
-langas.config(menu=meniu)
+note = Label(window, text="Iveskite vardą")
+entry = Entry(window)
+button = Button(window, text='Patvirtinti', fg="white", bg="pink", command=printing)
+window.bind('<Return>', lambda event: printing())
+result = Label(window, text="")
+
+button.bind("<Button-1>", printing)
+window.bind("<Return>", printing)
+
+note.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+button.grid(row=0, column=2)
+result.grid(row=1, columnspan=3)
+
+
+my_menu = Menu(window)
+window.config(menu=my_menu)
 submeniu = Menu(meniu, tearoff=0)
 
 def isvalyti():
-    rezultatas['text']=''
+    result['text']=''
 def atkurti_paskutini():
-    rezultatas['text'] = f'Labas,{issaugota.get()}!'
+    result['text'] = f'Labas,{saved.get()}!'
 def uzdaryti():
-    langas.destroy()
+    window.destroy()
 
-meniu.add_cascade(labe="Meniu", menu=submeniu)
-submeniu.add_command(label="Išvalyti", command=isvalyti)
-submeniu.add_command(label="Atkurti paskutinį", command=atkurti_paskutini)
-submeniu.add_separator()
-submeniu.add_command(label="Išeiti", command=uzdaryti)
+my_menu.add_cascade(labe="Meniu", menu=submenu)
+submenu.add_command(label="Išvalyti", command=isvalyti)
+submenu.add_command(label="Atkurti paskutinį", command=atkurti_paskutini)
+submenu.add_separator()
+submenu.add_command(label="Išeiti", command=uzdaryti)
 
 
-langas.mainloop()
+window.mainloop()
