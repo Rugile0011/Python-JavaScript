ce2e4147ebdeed5444faf5f13d8676ac261fd607
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:07:51 +0300

    Update 2.1.py

--- Homework/2.1.py
+++ Homework/2.1.py
@@ -1,4 +1,4 @@
-#sukurti zodyna ir sarasa
+
 d = {
     "brand": "iphone",
     "model": "X",
@@ -6,25 +6,24 @@ d = {
 
 s = [1, 2, 3, 4, 5]
 
-#atspausdinti reiksme
 print(s[0])
 print(d["brand"])
 
-#pridesti reiksme
+
 d["color"] = "black"
 print(d)
 
 s.append(6)
 print(s)
 
-#istrinti reiksme
+
 del d["brand"]
 print(d)
 
 del s[2]
 print(s)
 
-#pakeisti irasa
+
 
 s[0] = 0
 print(s)
