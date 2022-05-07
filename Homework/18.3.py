744a71bb58174602944ecf3a1d5c2e006efa52ff
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:08:44 +0300

    Update 18.3.py

--- Homework/18.3.py
+++ Homework/18.3.py
@@ -1,11 +1,11 @@
-sarasas = [2.5, 2, "Labas", True, 5, 7, 8, 2.8, "Vakaras"]
+list = [2.5, 2, "Hello", True, 5, 7, 8, 2.8, "Evening"]
 
-int_together = [x for x in sarasas if type(x) == int]
+int_together = [x for x in list if type(x) == int]
 print(sum(int_together))
 
-str_together = " ".join([x for x in sarasas if type(x) == str])
+str_together = " ".join([x for x in list if type(x) == str])
 print(str_together)
 
-B = sum(type(x) is bool for x in sarasas)
+B = sum(type(x) is bool for x in list)
 
 print(B)
