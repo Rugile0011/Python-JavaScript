97883695dcf5f384073f6178df2184502e4b0789
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:55:41 +0300

    Update 2.3.py

--- Homework/2.3.py
+++ Homework/2.3.py
@@ -1,22 +1,22 @@
-zodis = []
+word = []
 for i in range(5):
-    z = input("Iveskite zodi: \n")
+    z = input("Input word: \n")
     zodis.append(z)
-print(zodis)
+print(word)
 
 
-lenght_sentence0 = len(zodis[0])
-print("Pirmo zodzio ilgis:", lenght_sentence0)
-lenght_sentence1 = len(zodis[1])
-print("Antro zodzio ilgis:", lenght_sentence1)
-lenght_sentence2 = len(zodis[2])
-print("Trecio zodzio ilgis:", lenght_sentence2)
-lenght_sentence3 = len(zodis[3])
-print("Ketvirto zodzio ilgis:", lenght_sentence3)
-lenght_sentence4 = len(zodis[4])
-print("Penkto zodzio ilgis:", lenght_sentence4)
+lenght_sentence0 = len(word[0])
+print("First word lenght:", lenght_sentence0)
+lenght_sentence1 = len(word[1])
+print("Second word lenght:", lenght_sentence1)
+lenght_sentence2 = len(word[2])
+print("Third word lenght:", lenght_sentence2)
+lenght_sentence3 = len(word[3])
+print("Fourth word lenght:", lenght_sentence3)
+lenght_sentence4 = len(word[4])
+print("Fifth word lenght:", lenght_sentence4)
 
 n = 1
-for irasas in zodis:
-    print(n, irasas)
+for item in word:
+    print(n, item)
     n += 1
