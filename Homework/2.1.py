3f4943c90fddf8aa7a12c660b2d5fe7b1bcec933
Rugile <94554972+Rugile0011@users.noreply.github.com>
2022-05-07 18:52:58 +0300

    Update 2.1.py

--- Homework/2.1.py
+++ Homework/2.1.py
@@ -9,21 +9,17 @@ s = [1, 2, 3, 4, 5]
 print(s[0])
 print(d["brand"])
 
-
 d["color"] = "black"
 print(d)
 
 s.append(6)
 print(s)
 
-
 del d["brand"]
 print(d)
 
 del s[2]
 print(s)
-
-
 
 s[0] = 0
 print(s)
